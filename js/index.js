4abd5b0b5231fc608d9a14dcd0cb0d585636959f
Serhii Savchak <itsavchak@gmail.com>
2024-09-23 13:42:40 +0300

    add sidebar mobile adaptive styles

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log("hi");
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-sidebar-open]"),
+    closeMenuBtn: document.querySelector("[data-sidebar-close]"),
+    menu: document.querySelector("[data-sidebar]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle("is-open");
+  }
+})();
